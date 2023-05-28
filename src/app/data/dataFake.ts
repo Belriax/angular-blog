328bbe635e0a014413ac1e77680adc74e2904a1a
Belriax <gleiconsousa@gmail.com>
2023-05-28 16:51:32 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Kush",
+    "description": "A suprima arte do marroquino",
+    "photoCover": "https://shrm-res.cloudinary.com/image/upload/c_crop,h_2565,w_4563,x_0,y_104/w_auto:100,w_1200,q_35,f_auto/v1/Magazine/iStock-1442479025_nrh1c9.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Colombia",
+    "description": "O famoso Colombia Gold",
+    "photoCover": "https://blog.tabacariadamata.com.br/wp-content/uploads/2023/02/damata-colombian-gold-acapulco-gold.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Colombia",
+    "description": "O famoso Colombia Gold",
+    "photoCover": "https://i0.wp.com/hospitalsantamonica.com.br/wp-content/uploads/2019/12/dossie-maconha.jpg?fit=1920%2C1280&ssl=1"
+  }
+]
